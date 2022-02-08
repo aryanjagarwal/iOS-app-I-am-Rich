7e02e8038f86bf8a03161e84aa9efd08e322f038
Aryan Jagarwal <ajbaggar@gmail.com>
2022-02-08 15:19:51 +0530

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Aryan Jagarwal on 2/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
